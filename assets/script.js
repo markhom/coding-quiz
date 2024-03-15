a078244aab3246c9a511b4c66343d229a0d9df4d
Mark Homchik <markhomchik4@gmail.com>
2024-03-14 21:56:53 -0500

    added questions and answers

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+        question: "Which CSS property is used to change the text color of an element?",
+        options: ["'background-color'", "'color'", "'text-color'", "'font-color'" ],
+        correctAnswer: "'color'"
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyper Text Markup Language", "High Text Markup Language", "Hello Tony Marcus Lounge", "Home Tool Markup Language"],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+    {
+        question: "Which tag is used to define an unordered list in HTML?",
+        options: ["'<ol>'", "'<ul>'", "'<il>'", "'<list>'"],
+        correctAnswer: "'<ul>'"
+    },
+    {
+        question: "Which programming language is used for building dynamic web applications?",
+        options: ["Java", "Python", "C#", "JavaScript"],
+        correctAnswer: "JavaScript"
+    },
+    {
+        question: "True or False: JavaScript is a case-sensitive language.",
+        options: ["True", "False"],
+        correctAnswer: "True"
+    },
+    {
+        question: "Which of the following is not a JavaScript data type?",
+        options: ["Float", "String", "Boolean", "Object"],
+        correctAnswer: "Float"
+    },
+    {
+        question: "Which symbol is used for single-line comments in JavaScript?",
+        options: ["'///'", "'/|\'", "'!?//'", "'//'"],
+        correctAnswer: "'//'"
+    },
+    {
+        question: "Which of the following is NOT a valid way to declare a variable in JavaScript?",
+        options: ["'var x = 5'", "'let y = 6'", "'const z = 4'", "'variable v = 9'"],
+        correctAnswer: "'variable v = 9'"
+    },
+    {
+        question: "True or False: HTML is a programming language.",
+        options: ["True", "False"],
+        correctAnswer: "False"
+    },
+    {
+        question: "What does CSS stand for?",
+        options: ["Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Colorful Style Sheets"],
+        correctAnswer: "Cascading Style Sheets"
+    }
+
+]
