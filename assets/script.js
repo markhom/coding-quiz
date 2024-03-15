4075cd230b0bfa961737b2a98276c43acad5644d
Mark Homchik <markhomchik4@gmail.com>
2024-03-14 22:41:57 -0500

    functions and vars for quiz to work

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,56 @@
+// Identifying timer score and current question
+let timer = 100
+let score = 0
+let currentQuestion = 0
+
+const startButton = document.getElementById("btn");
+const questionContainer = document.getElementById('question-container');
+const scoreContainer = document.getElementById('score-container');
+
+// When the start button is clicked, the startQuiz function is called
+startButton.addEventListener("click", startQuiz);
+
+// Removes start button from the display and runs the next question function
+function startQuiz(){
+    startButton.style.display = 'none';
+    displayNextQuestion();
+};
+
+// Function that displays the questions
+function displayNextQuestion(){
+    if(currentQuestion < questions.length) {
+        const question = questions[currentQuestion];
+        questionContainer.innerHTML = `
+            <h2>${question.question}</h2>
+            <ul>
+                ${question.options.map(option => `<li><button onclick="checkAnswer('${option}')">${option}</button></li>`).join('')}
+            </ul>
+        `;
+    } else {
+        endQuiz
+    }
+};
+
+// Function that ends the quiz
+function endQuiz(){
+    questionContainer.innerHTML = 'Quiz ended!';
+    scoreContainer.innerHTML = `Your score: ${score}`;
+}
+
+// Function that checks score
+function checkScore(selectedAnswer){
+    const question = questions[currentQuestionIndex];
+    if (selectedAnswer === question.correctAnswer) {
+        score++;
+    } else {
+        timer -= 10; // Decrease timer by 10 seconds for incorrect answer
+    }
+    currentQuestionIndex++;
+    displayNextQuestion();
+}
+
+
+// All Questions
 const questions = [
     {
         question: "Which CSS property is used to change the text color of an element?",
